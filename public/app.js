87405c8e68934269b1fe29b67dcf4c45d02ff787
Ben Green <ben@latenightsketches.com>
2016-09-18 06:55:56 +0000

    Beginning renderer

--- public/app.js
+++ public/app.js
@@ -1,13 +1,41 @@
-const app = new ApiServer('http://localhost:3000');
 
 const debug = promise => promise
   .then(result=>console.log(result))
   .catch(reason=>console.error(reason));
 
-// debug(app.signup('you@example.com','super_secret'))
-// debug(app.validateEmail('you@example.com','from the events log'))
-// debug(app.login('you@example.com','super_secret'))
-// debug(app.createPost('hello, world!','oldest trick in the book'))
-// debug(app.getPosts())
-// debug(app.updatePost(1, 'worm', 'not always'))
-// debug(app.deletePost(1))
+// debug(app.server.signup('you@example.com','super_secret'))
+// debug(app.server.validateEmail('you@example.com','from the events log'))
+// debug(app.server.login('you@example.com','super_secret'))
+// debug(app.server.createPost('hello, world!','oldest trick in the book'))
+// debug(app.server.getPosts())
+// debug(app.server.updatePost(1, 'worm', 'not always'))
+// debug(app.server.deletePost(1))
+
+class BlogApp {
+  constructor(url) {
+    this.element = document.createElement('div');
+    this.server = new ApiServer(url);
+
+    this.render();
+  }
+  render() {
+    return Promise.all([
+      this.server.getPosts()
+    ]).then(results => {
+      const [posts] = results;
+      this.element.innerHTML = html`
+        <ul>${posts.map(post => html`
+          <li>
+            <div class="title">$${post.title}</div>
+            <div class="author">$${post.author}</div>
+            <div class="created_at">$${post.created_at}</div>
+            <div class="body">$${post.body}</div>
+          </li>`)}
+        </ul>
+      `;
+    });
+  }
+}
+
+const app = new BlogApp('http://localhost:3000');
+document.body.appendChild(app.element);
---
+++ public/htmlEscape.js
@@ -0,0 +1,24 @@
+// From http://www.2ality.com/2015/01/template-strings-html.html
+// Use html`$${<expression>}` to escape HTML
+function html(literals, ...substs) {
+  return literals.raw.reduce((acc, lit, i) => {
+    let subst = substs[i-1];
+    if (Array.isArray(subst)) {
+        subst = subst.join('');
+    }
+    if (acc.endsWith('$')) {
+        subst = htmlEscape(subst);
+        acc = acc.slice(0, -1);
+    }
+    return acc + subst + lit;
+  });
+}
+
+function htmlEscape(str) {
+  return str.replace(/&/g, '&amp;') // first!
+            .replace(/>/g, '&gt;')
+            .replace(/</g, '&lt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/`/g, '&#96;');
+}
